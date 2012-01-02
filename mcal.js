8575992e9d19e26b9257a16a1093e22f9a2ae206
Jesse D Zbikowski <embeddedlinuxguy@gmail.com>
2012-01-02 16:46:51 -0500

    Move model setup into init()

--- mcal.js
+++ mcal.js
@@ -1,5 +1,31 @@
-var glyph = 1;
-var min_glyph = 2;
+var mcal = {
+    mmonth : 1,
+    mdate : 1,
+    mhr : 1,
+    mmin : 1,
+    msec : 1,
+    mjif : 1
+};
+
+var maya_month = [ "Imix", "Ik", "Akbal" ];
+
+function init_time () {
+    var ny = 1324465860; //13.20.20 1.1 1.1.1 === 1324465860
+    var d = new Date();
+    var s = Math.floor(d.getTime()/1000) - ny;
+    var t = s % 86400;
+    
+    mcal.mhr = 1 + Math.floor(t / 4320);
+
+    s = Math.floor(s/86400);
+    mcal.mdate = 1 + s % 20;
+    s = Math.floor(s/20);
+    mcal.mmonth = maya_month[s % 20];
+    
+    document.getElementById("myear").innerHTML = "13 . 20 . 19";
+    document.getElementById("mdate").innerHTML = mcal.mmonth + " " + mcal.mdate;
+    document.getElementById("mtime").innerHTML = mcal.mhr+" . "+mcal.mmin+" . "+mcal.msec;
+}
 
 function do_time() {
   mkTime();
@@ -7,15 +33,20 @@ function do_time() {
 }
 
 function do_glyph () {
-  if (glyph === 20) {
-    min_glyph = 1 + (min_glyph % 20);
-    var shown_glyph = 1 + (min_glyph % 6);
-    document.getElementById("glyph_minutes").src = "maya/" + shown_glyph + ".png";
-  }
-  glyph = 1 + (glyph % 20);
-  var shown_glyph = 1 + (glyph % 6);
-  document.getElementById("glyph_seconds").src = "maya/" + shown_glyph + ".png";
-  window.setTimeout(do_glyph, 540);
+    if (mcal.mjif === 20) {
+	if (mcal.msec === 20) {
+	    mcal.mmin = 1 + (mcal.mmin % 20);
+	}
+	mcal.msec = 1 + (mcal.msec % 20);
+	// XXX minutes -> seconds
+	document.getElementById("mtime").innerHTML = [ mcal.mhr, mcal.mmin, mcal.msec ].join(" . ");
+	document.getElementById("glyph_minutes").src = "maya/" + mcal.msec + ".png";
+    }
+    mcal.mjif = 1 + (mcal.mjif % 20);
+    // XXX seconds -> jiffies
+    document.getElementById("glyph_seconds").src = "maya/" + mcal.mjif + ".png";
+
+    window.setTimeout(do_glyph, 540);
 }
 
 var mkDate = (function () {
@@ -41,11 +72,15 @@ var mkTime = function () {
   document.getElementById("time").innerHTML = d.getHours() + ":" + m + ":" + s;
 }
 
+init_time();
+
 mkYear();
 mkDate();
 mkTime();
 do_glyph();  
 do_time();
-document.getElementById("myear").innerHTML = "13 . 20 . 19";
-document.getElementById("mdate").innerHTML = "Yax 17"
-document.getElementById("mtime").innerHTML = "20 . 20 . 18";
+
+
+//December 21 11:11:00am GMT 2012
+//=== 1356088260
+//=== 14.1.1 1.1 1.1.1
